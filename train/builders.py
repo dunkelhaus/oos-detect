9f61cdd7460160644f9ba11e4608611b5e846230
Suraj Jena <jena.suraj.k@gmail.com>
2020-08-04 00:34:58 -0700

    Updated hyperparams, updated embedder structure to fix incorrect masks

--- train/builders.py
+++ train/builders.py
@@ -7,10 +7,12 @@ from typing import List, Dict, Tuple, Iterable
 from datasets.readers.oos_eval import OOSEvalReader
 from models.single_layer_lstm import SingleLayerLSTMClassifier
 from allennlp.training.optimizers import HuggingfaceAdamWOptimizer
+from allennlp.modules.seq2vec_encoders import PytorchSeq2VecWrapper
 from allennlp.modules.seq2vec_encoders.bert_pooler import BertPooler
 from allennlp.training.trainer import Trainer, GradientDescentTrainer
 from allennlp.data import DatasetReader, DataLoader, Instance, Vocabulary
 from allennlp.modules.token_embedders import PretrainedTransformerEmbedder
+from allennlp.modules.text_field_embedders import TextFieldEmbedder, BasicTextFieldEmbedder
 
 # Logger setup.
 log = logging.getLogger(__name__)
@@ -45,9 +47,13 @@ def build_model(vocab: Vocabulary) -> Model:
     """
     log.debug("Building the model.")
     vocab_size = vocab.get_vocab_size("tokens")
-    embedder = PretrainedTransformerEmbedder("bert-base-uncased")
+    bert_embedder = PretrainedTransformerEmbedder("bert-base-uncased")
+    embedder: TextFieldEmbedder = BasicTextFieldEmbedder(
+        {"tokens": bert_embedder}
+    )
     log.debug("Embedder built.")
     encoder = BertPooler("bert-base-uncased", requires_grad=True)
+    # encoder = PytorchSeq2VecWrapper(torch.nn.LSTM(768,20,batch_first=True))
     log.debug("Encoder built.")
     return SingleLayerLSTMClassifier(vocab, embedder, encoder).cuda(0)
 
@@ -55,6 +61,7 @@ def build_model(vocab: Vocabulary) -> Model:
 def build_data_loaders(
     train_data: torch.utils.data.Dataset,
     dev_data: torch.utils.data.Dataset,
+    batch_size: int
 ) -> Tuple[DataLoader, DataLoader]:
     """
     Build the AllenNLP DataLoaders.
@@ -70,12 +77,12 @@ def build_data_loaders(
     log.debug("Building DataLoaders.")
     train_loader = PyTorchDataLoader(
         train_data,
-        batch_size=8,
-        shuffle=False
+        batch_size=batch_size,
+        shuffle=True
     )
     dev_loader = PyTorchDataLoader(
         dev_data,
-        batch_size=8,
+        batch_size=batch_size,
         shuffle=False
     )
     log.debug("DataLoaders built.")
@@ -86,7 +93,9 @@ def build_trainer(
     model: Model,
     serialization_dir: str,
     train_loader: DataLoader,
-    dev_loader: DataLoader
+    dev_loader: DataLoader,
+    lr: float,
+    num_epochs: int
 ) -> Trainer:
     """
     Build the model trainer. Includes instantiating the optimizer as well.
@@ -102,13 +111,13 @@ def build_trainer(
         [n, p]
         for n, p in model.named_parameters() if p.requires_grad
     ]
-    optimizer = HuggingfaceAdamWOptimizer(parameters, lr=1.0e-5)
+    optimizer = HuggingfaceAdamWOptimizer(parameters, lr=lr)
     trainer = GradientDescentTrainer(
         model=model,
         serialization_dir=serialization_dir,
         data_loader=train_loader,
         validation_data_loader=dev_loader,
-        num_epochs=5,
-        optimizer=optimizer,
+        num_epochs=num_epochs,
+        optimizer=optimizer
     )
     return trainer
