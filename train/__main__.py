c5b0bfcf401aa1bdc4e81944e78a84c639ff37c1
Suraj Jena <jena.suraj.k@gmail.com>
2020-08-04 00:35:22 -0700

    Updated for training--temporary

--- train/__main__.py
+++ train/__main__.py
@@ -6,6 +6,7 @@ logging.config.dictConfig(LOGGING_CONFIG)
 logging.getLogger("transformers").setLevel(logging.ERROR)
 # logging.getLogger("transformers").setLevel(logging.DEBUG)
 
+import wandb
 from allennlp.data import DataLoader
 from train.loop import run_training_loop
 from allennlp.training.util import evaluate
@@ -17,15 +18,22 @@ log = logging.getLogger(__name__)
 log.debug("Logging is configured.")
 
 
+wandb.init(entity="dunkelhaus", project="oos-detect")
+
+
 # We've copied the training loop from an earlier example, with updated model
 # code, above in the Setup section. We run the training loop to get a trained
 # model.
 model, dataset_reader = run_training_loop()
 
 # Now we can evaluate the model on a new dataset.
-test_data = dataset_reader.read(locate_oos_data()/"data_full_test.json")
+"""test_data = dataset_reader.read(locate_oos_data()/"data_full_test.json")
 test_data.index_with(model.vocab)
-data_loader = DataLoader(test_data, batch_size=8)
+data_loader = DataLoader(
+    test_data,
+    batch_size=8,
+    shuffle=False
+)
 
 results = evaluate(model, data_loader)
-print(results)
+print(results)"""
