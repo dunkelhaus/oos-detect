cb1805d9ed9ae551167c2de61e0c28ac16ac6960
Suraj Jena <jena.suraj.k@gmail.com>
2020-08-04 00:34:31 -0700

    Add wandb config to train loop

--- train/loop.py
+++ train/loop.py
@@ -1,3 +1,6 @@
+import os
+import wandb
+import torch
 import logging
 from train.builders import build_model
 from train.builders import build_vocab
@@ -25,6 +28,14 @@ def read_data(
 
 
 def run_training_loop():
+    wandb.init(entity="dunkelhaus", project="oos-detect")
+    wbconf = wandb.config
+
+    wbconf.batch_size = 64
+    wbconf.lr = 0.0001
+    wbconf.num_epochs = 5
+    wbconf.no_cuda = False
+    wbconf.log_interval = 10
     dataset_reader = build_dataset_reader()
 
     # These are a subclass of pytorch Datasets, with some allennlp-specific
@@ -33,6 +44,8 @@ def run_training_loop():
 
     vocab = build_vocab(train_data + dev_data)
     model = build_model(vocab)
+
+    wandb.watch(model, log="all")
 
     # This is the allennlp-specific functionality in the Dataset object;
     # we need to be able convert strings in the data to integers, and this
@@ -43,7 +56,7 @@ def run_training_loop():
     # These are again a subclass of pytorch DataLoaders, with an
     # allennlp-specific collate function, that runs our indexing and
     # batching code.
-    train_loader, dev_loader = build_data_loaders(train_data, dev_data)
+    train_loader, dev_loader = build_data_loaders(train_data, dev_data, wbconf.batch_size)
 
     # Locate serialization directory.
     serialization_dir = locate_results_dir()
@@ -58,8 +71,12 @@ def run_training_loop():
         model,
         serialization_dir,
         train_loader,
-        dev_loader
+        dev_loader,
+        lr=wbconf.lr,
+        num_epochs=wbconf.num_epochs
     )
+
     trainer.train()
+    torch.save(model.state_dict(), os.path.join(wandb.run.dir, "linear-bert-uncased.pt"))
 
     return model, dataset_reader
