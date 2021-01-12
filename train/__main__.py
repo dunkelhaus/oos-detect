474a2f0183fd818d03492dc9d8bce034fc8a42d1
Suraj Jena <jena.suraj.k@gmail.com>
2021-01-12 00:00:45 -0800

    Playing with allennlp Predictor

--- train/__main__.py
+++ train/__main__.py
@@ -7,6 +7,7 @@
 # logging.getLogger("transformers").setLevel(logging.DEBUG)
 
 # import wandb
+import numpy as np
 from train.loop import run_training_loop
 from utilities.locate import locate_oos_data
 from allennlp.predictors import TextClassifierPredictor
@@ -23,19 +24,42 @@ from allennlp.predictors import TextClassifierPredictor
 # a trained model.
 
 
-model, dataset_reader = run_training_loop(run_test=True)
+model, dataset_reader = run_training_loop(run_test=False)
 
 predictor = TextClassifierPredictor(
     model=model,
     dataset_reader=dataset_reader
 )
 
-preds = predictor.predict_batch_instance(
-            list(
-                dataset_reader.read(
-                    locate_oos_data()/"data_full_test.json"
-                )
-            )
-        )
+test_data = list(
+    dataset_reader.read(
+        locate_oos_data()/"data_full_test.json"
+    )
+)
 
-print(preds)
+print(f"Example test instance: {test_data[0]}.")
+
+preds = predictor.predict_batch_instance(test_data)
+
+labelmap = predictor._model.vocab.get_index_to_token_vocabulary('labels')
+
+print(f"Label map: {labelmap}")
+
+print(f"Instance predictions made: {len(preds)}.")
+print(f"First prediction: {preds[0]}")
+
+# Logits to labels.
+for l in preds:
+    # print(l)
+    argind = np.argmax(l['probs'])
+    # print(argind)
+    lab = labelmap[argind]
+    # print(lab)
+    l['predicted'] = lab
+
+#preds['predicted'] = [labelmap[np.argmax(l['probs'])] for l in preds]
+
+# pr = predictor.predictions_to_labeled_instances(test_data[0], preds[0])
+
+for i in range(len(preds)):
+    print(f"Instance: {test_data[i]}; Prediction: {preds[i]['predicted']}")
