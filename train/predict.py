1c6abdc79a7f006001b484ebb787c9af41004588
Suraj Jena <jena.suraj.k@gmail.com>
2021-01-13 13:16:49 -0800

    Batching with bound of 4k added for predictions (out of vram)

--- train/predict.py
+++ train/predict.py
@@ -9,7 +9,27 @@ def get_predictions(model, dataset_reader, data):
         dataset_reader=dataset_reader
     )
 
-    preds = predictor.predict_batch_instance(data)
+    size = len(data)
+    bound = 4000
+    preds = []
+
+    if size > bound:
+        times = int(size/bound)
+        print(f"Set is too big; total size: {size}. "
+                f"Batching {times} times".)
+
+        for i in range(times):
+            print(f"Lower: {bound*i}, Upper: {bound*(i + 1)}")
+            preds += predictor.predict_batch_instance(
+                data[bound*i:bound*(i+1)]
+            )
+
+        if (size - (bound * times)) > 0:
+            print(f"Lower: {bound*times}, Upper: {size}")
+            preds += predictor.predict_batch_instance(data[bound*times:])
+    else:
+        preds = predictor.predict_batch_instance(data)
+
     labelmap = predictor._model.vocab.get_index_to_token_vocabulary('labels')
 
     predictions = [labelmap[np.argmax(l['probs'])] for l in preds]
