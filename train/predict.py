507c109a32b68430ac955736089326ce26953a94
Suraj Jena <jena.suraj.k@gmail.com>
2021-01-13 12:38:11 -0800

    Updated predict to be dataset independent

--- train/predict.py
+++ train/predict.py
@@ -3,23 +3,17 @@ from utilities.locate import locate_oos_data
 from allennlp.predictors import TextClassifierPredictor
 
 
-def get_test_predictions(model, dataset_reader):
+def get_predictions(model, dataset_reader, data):
     predictor = TextClassifierPredictor(
         model=model,
         dataset_reader=dataset_reader
     )
 
-    test_data = list(
-        dataset_reader.read(
-            locate_oos_data()/"data_full_test.json"
-        )
-    )
-
-    preds = predictor.predict_batch_instance(test_data)
+    preds = predictor.predict_batch_instance(data)
     labelmap = predictor._model.vocab.get_index_to_token_vocabulary('labels')
 
     predictions = [labelmap[np.argmax(l['probs'])] for l in preds]
-    actuals = [str(i['label'].label) for i in test_data]
+    actuals = [str(i['label'].label) for i in data]
     labels = list(labelmap.values())
 
     return actuals, predictions, labels
