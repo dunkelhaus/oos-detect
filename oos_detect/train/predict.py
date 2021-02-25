d15256090105fada6960dc25b48f152860ca9d19
Suraj Jena <jena.suraj.k@gmail.com>
2021-02-25 00:26:39 -0800

    Update to allennlp==2.1.0: Fixes to accomodate new dataloader format

--- oos_detect/train/predict.py
+++ oos_detect/train/predict.py
@@ -3,11 +3,12 @@ from oos_detect.utilities.locate import locate_oos_data
 from allennlp.predictors import TextClassifierPredictor
 
 
-def get_predictions(model, dataset_reader, data):
+def get_predictions(model, data_reader, data_path):
     predictor = TextClassifierPredictor(
         model=model,
-        dataset_reader=dataset_reader
+        dataset_reader=data_reader
     )
+    data = list(data_reader.read(data_path))
 
     size = len(data)
     bound = 4000
@@ -16,7 +17,7 @@ def get_predictions(model, dataset_reader, data):
     if size > bound:
         times = int(size/bound)
         print(f"Set is too big; total size: {size}. "
-                f"Batching {times} times.")
+              f"Batching {times} times.")
 
         for i in range(times):
             print(f"Lower: {bound*i}, Upper: {bound*(i + 1)}")
@@ -32,7 +33,10 @@ def get_predictions(model, dataset_reader, data):
 
     labelmap = predictor._model.vocab.get_index_to_token_vocabulary('labels')
 
-    predictions = [labelmap[np.argmax(l['probs'])] for l in preds]
+    predictions = [
+        labelmap[np.argmax(lst['probs'])]
+        for lst in preds
+    ]
     actuals = [str(i['label'].label) for i in data]
     labels = list(labelmap.values())
 
