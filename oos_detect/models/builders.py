1c82f98cbfae6d6387791cc7aa0d83e61c8553cd
Suraj Jena <jena.suraj.k@gmail.com>
2021-02-24 22:51:04 -0800

    Upgrade to allennlp==2.1.0: Vocab can be initialized from transformer + Data loader changed + New common callback + Trainer builder function

--- oos_detect/models/builders.py
+++ oos_detect/models/builders.py
@@ -1,69 +1,52 @@
 import torch
 from allennlp.models import Model
+from allennlp.data import Instance
 from allennlp.data import Vocabulary
 from allennlp.data import DataLoader
 from typing import Any, Tuple, Iterable
-from allennlp.data import PyTorchDataLoader
-from allennlp.data import DatasetReader, Instance
-from allennlp.training.trainer import EpochCallback
-from allennlp.training.trainer import BatchCallback
-from oos_detect.utilities.wandb_loggers import LogMetricsToWandb
-from oos_detect.utilities.wandb_loggers import LogBatchMetricsToWandb
-from allennlp.data.token_indexers import PretrainedTransformerIndexer
+from allennlp.training.trainer import Trainer
+from allennlp.data import MultiProcessDataLoader
+from allennlp.training.trainer import TrainerCallback
+from oos_detect.train.callbacks import LogMetricsToWandb
+from allennlp.training.trainer import GradientDescentTrainer
+from allennlp.training.optimizers import HuggingfaceAdamWOptimizer
 
 
-def build_epoch_callbacks(wbrun) -> EpochCallback:
+def build_callbacks(wbrun) -> TrainerCallback:
     """
     Instantiate callback - factory method.
 
+    :param wbrun: WandB object.
     :return LogMetricsToWandb: Instantiated LogMetricsToWandb object.
     """
 
     return [LogMetricsToWandb(wbrun=wbrun)]
 
 
-def build_batch_callbacks(wbrun) -> BatchCallback:
-    """
-    Instantiate callback - factory method.
-
-    :return LogBatchMetricsToWandb: Instantiated LogBatchMetricsToWandb object.
-    """
-
-    return [LogBatchMetricsToWandb(wbrun=wbrun)]
-
-
 def build_vocab(
-        instances: Iterable[Instance]
+        instances: Iterable[Instance],
+        from_transformer: bool = False
 ) -> Vocabulary:
     """
-    Build the Vocabulary object from the instances.
+    Build the Vocabulary object from the instances only,
+    or from the pretrained transformer, based on boolean flag
 
     :param instances: Iterable of allennlp instances.
+    :param from_transformer: Whether to initialize vocab from
+                             pretrained transformer, or from
+                             instances directly.
     :return Vocabulary: The Vocabulary object.
     """
     # log.debug("Building the vocabulary.")
     print(f"Adding {len(instances)} instances data to vocab.")
-    vocab = Vocabulary.from_instances(instances)
 
-    return vocab
-
-
-# HACK: Temporary inelegant solution; until release brings fix
-# (PR: #4958).
-def build_vocab_and_apply_transformer_vocab(
-        instances: Iterable[Instance],
-        indexer: PretrainedTransformerIndexer
-) -> Vocabulary:
-    """
-    Build the Vocabulary object from the instances.
-    Also calls indexer._add_encoding_to_vocabulary_if_needed() on
-    the vocab.
-
-    :param instances: Iterable of allennlp instances.
-    :return Vocabulary: The Vocabulary object.
-    """
-    vocab = build_vocab(instances)
-    indexer._add_encoding_to_vocabulary_if_needed(vocab)
+    if from_transformer:
+        vocab = Vocabulary.from_pretrained_transformer(
+            model_name="bert-base-uncased"
+        )
+        vocab.extend_from_instances(instances)
+    else:
+        vocab = Vocabulary.from_instances(instances)
 
     return vocab
 
@@ -85,7 +68,7 @@ def build_data_loader(
     # We need to get the allennlp-specific collate function, which is
     # what actually does indexing and batching.
     # log.debug("Building DataLoader.")
-    loader = PyTorchDataLoader(
+    loader = MultiProcessDataLoader(
         data,
         batch_size=batch_size,
         shuffle=True
@@ -122,3 +105,47 @@ def build_train_data_loaders(
     # log.debug("Training DataLoaders built.")
 
     return train_loader, dev_loader
+
+
+def build_grad_desc_with_adam_trainer(
+        model: Model,
+        serialization_dir: str,
+        train_loader: DataLoader,
+        dev_loader: DataLoader,
+        lr: float,
+        num_epochs: int,
+        wbrun: Any = None
+) -> Trainer:
+    """
+    Build the model trainer.
+    Includes instantiating the optimizer as well.
+    This builder uses the GradientDescentTrainer &
+    HuggingfaceAdamWOptimizer combo.
+    Also allows setting callbacks (atm for WandB mainly).
+
+    :param model: The model object to be trained.
+    :param serialization_dir: The serialization directory to output
+            results to.
+    :param train_loader: The training data loader.
+    :param dev_loader: The dev data loader.
+    :param lr: Learning rate.
+    :param num_epochs: Number of epochs to train for.
+    :param wbrun: WandB object to use for callbacks.
+    :return trainer: The Trainer object.
+    """
+    parameters = [
+        [n, p]
+        for n, p in model.named_parameters() if p.requires_grad
+    ]
+    optimizer = HuggingfaceAdamWOptimizer(parameters, lr=lr)
+    trainer = GradientDescentTrainer(
+        model=model,
+        serialization_dir=serialization_dir,
+        data_loader=train_loader,
+        validation_data_loader=dev_loader,
+        num_epochs=num_epochs,
+        optimizer=optimizer,
+        callbacks=build_callbacks(wbrun) if wbrun else None
+    )
+
+    return trainer
